498ba51b98bcff2371b0033859d2460049898c82
Zeeshanp23 <130479030+Zeeshanp23@users.noreply.github.com>
2023-10-28 09:43:54 +0500

    tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -1,94 +1,149 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-// initialize the game board
-vector<string> board = {"-", "-", "-", "-", "-", "-", "-", "-", "-"};
+char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
-// define game functions
-void display_board() {
-    cout << board[0] << " | " << board[1] << " | " << board[2] << endl;
-    cout << "--+---+--" << endl;
-    cout << board[3] << " | " << board[4] << " | " << board[5] << endl;
-    cout << "--+---+--" << endl;
-    cout << board[6] << " | " << board[7] << " | " << board[8] << endl;
-}
+int checkwin();
+void board();
 
-int player_input(string player) {
-    int move;
-    cout << "Player " << player << ", enter your move (1-9):";
-    cin >> move;
-    while (move < 1 || move > 9 || board[move-1] != "-") {
-        cout << "Invalid move, please try again:";
-        cin >> move;
-    }
-    return move-1;
-}
+int main()
+{
+	int player = 1,i,choice;
 
-bool check_win(string player) {
-    vector<vector<int>> win_patterns = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    for (vector<int> pattern : win_patterns) {
-        if (board[pattern[0]] == player && board[pattern[1]] == player && board[pattern[2]] == player) {
-            return true;
+    char mark;
+    do
+    {
+        board();
+        player=(player%2)?1:2;
+
+        cout << "Player " << player << ", enter a number:  ";
+        cin >> choice;
+
+        mark=(player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && square[1] == '1')
+
+            square[1] = mark;
+        else if (choice == 2 && square[2] == '2')
+
+            square[2] = mark;
+        else if (choice == 3 && square[3] == '3')
+
+            square[3] = mark;
+        else if (choice == 4 && square[4] == '4')
+
+            square[4] = mark;
+        else if (choice == 5 && square[5] == '5')
+
+            square[5] = mark;
+        else if (choice == 6 && square[6] == '6')
+
+            square[6] = mark;
+        else if (choice == 7 && square[7] == '7')
+
+            square[7] = mark;
+        else if (choice == 8 && square[8] == '8')
+
+            square[8] = mark;
+        else if (choice == 9 && square[9] == '9')
+
+            square[9] = mark;
+        else
+        {
+            cout<<"Invalid move ";
+
+            player--;
+            cin.ignore();
+            cin.get();
         }
-    }
-    return false;
-}
+        i=checkwin();
 
-bool check_draw() {
-    for (string cell : board) {
-        if (cell == "-") {
-            return false;
-        }
-    }
-    return true;
-}
+        player++;
+    }while(i==-1);
+    board();
+    if(i==1)
 
-int main() {
-    // main game loop
-    string player = "X";
-    display_board();
-    while (true) {
-        int move = player_input(player);
-        board[move] = player;
-        display_board();
-        if (check_win(player)) {
-            cout << "Player " << player << " wins!" << endl;
-            break;
-        } else if (check_draw()) {
-            cout << "Game is a draw." << endl;
-            break;
-        }
-        player = (player == "X") ? "O" : "X";
-    }
+        cout<<"==>\aPlayer "<<--player<<" win ";
+    else
+        cout<<"==>\aGame draw";
 
-    // ask to play again
-    char play_again;
-    cout << "Play again? (y/n)";
-    cin >> play_again;
-    while (play_again == 'y') {
-        board = {"-", "-", "-", "-", "-", "-", "-", "-", "-"};
-        player = "X";
-        display_board();
-        while (true) {
-            int move = player_input(player);
-            board[move] = player;
-            display_board();
-            if (check_win(player)) {
-                cout << "Player " << player << " wins!" << endl;
-                break;
-            } else if (check_draw()) {
-                cout << "Game is a draw." << endl;
-                break;
-            }
-            player = (player == "X") ? "O" : "X";
-        }
-        cout << "Play again? (y/n)";
-        cin >> play_again;
-    }
-
-    cout << "Thanks for playing!" << endl;
+    cin.ignore();
+    cin.get();
     return 0;
 }
+
+/*********************************************
+    FUNCTION TO RETURN GAME STATUS
+    1 FOR GAME IS OVER WITH RESULT
+    -1 FOR GAME IS IN PROGRESS
+    O GAME IS OVER AND NO RESULT
+**********************************************/
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+
+        return 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+
+        return 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' 
+                    && square[4] != '4' && square[5] != '5' && square[6] != '6' 
+                  && square[7] != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return -1;
+}
+
+
+/*******************************************************************
+     FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+********************************************************************/
+
+
+void board()
+{
+    system("cls");
+    cout << "\n\n\tTic Tac Toe\n\n";
+
+    cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+    cout << endl;
+
+    cout << "     |     |     " << endl;
+    cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+
+    cout << "     |     |     " << endl << endl;
+}
+
+/*******************************************************************
+                END OF PROJECT
+********************************************************************/
